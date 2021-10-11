2d16f9d3f72c4691eb3d0a38a529e4852d3c3948
Rajat Rajput <rajputrajat@gmail.com>
2021-10-11 23:36:08 +0530

    only process configs for tags

--- src/main.rs
+++ src/main.rs
@@ -23,7 +23,7 @@ async fn main() -> Result<()> {
         None,
     )?;
     let list = cmd.list_parallel(&svn_path, |svn_url, entry| {
-        let config_str = [("configuration.xml", true), ("Variation", false)];
+        let config_str = [("tags", true), ("Variation", false)];
         if config_str.iter().all(|i| i.1 == svn_url.contains(i.0)) {
             let time = DateTime::parse_from_rfc3339(&entry.commit.date).unwrap();
             if time.date().year() >= 2021 {
@@ -39,14 +39,16 @@ async fn main() -> Result<()> {
     let mut config_handler = ConfigFiles::new().await?;
     for e in list.lock().unwrap().iter() {
         let path = format!("{}/{}", e.0 .0, e.1.name);
-        async {
-            let file_text = cmd.cat(&path).await.unwrap();
-            config_handler
-                .save_new_file(&path, &file_text)
-                .await
-                .unwrap();
+        if path.contains("configuration.xml") {
+            async {
+                let file_text = cmd.cat(&path).await.unwrap();
+                config_handler
+                    .save_new_file(&path, &file_text)
+                    .await
+                    .unwrap();
+            }
+            .await;
         }
-        .await;
     }
     config_handler.set_db().await?;
     Ok(())
