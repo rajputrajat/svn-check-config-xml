3efb635bcb65cd16ff43dbbac4e37dfd2c1d4548
Rajat Rajput <rajputrajat@gmail.com>
2021-10-11 18:12:36 +0530

    getter and setter are ready

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use anyhow::Result;
-use async_std::{fs::File, path::Path};
+use async_std::{
+    fs::{self, File},
+    path::{Path, PathBuf},
+};
 use serde::{Deserialize, Serialize};
 use std::{env, time::Instant};
 use svn_cmd::{Credentials, SvnCmd};
@@ -18,6 +21,7 @@ async fn main() -> Result<()> {
         None,
     )?;
     let dir = Path::new(&env::var("USERPROFILE")?).join("config_xmls");
+    let db = dir.join("map.toml");
     let list = cmd.list_parallel(&svn_path)?;
     println!(
         "time took with SVN: {:#?} msec",
@@ -35,5 +39,35 @@ async fn main() -> Result<()> {
         "time took: {:#?} msec",
         Instant::now().duration_since(instant).as_millis()
     );
+    Ok(())
+}
+
+#[derive(Serialize, Deserialize)]
+struct DbConfig {
+    pairs: Vec<Pairs>,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Pairs {
+    name: String,
+    file_path: String,
+}
+
+async fn get_db(path: &str) -> Result<DbConfig> {
+    match fs::read_to_string(path).await {
+        Ok(t) => {
+            let db = toml::from_str::<DbConfig>(&t)?;
+            Ok(db)
+        }
+        Err(_) => {
+            File::create(path).await?;
+            Ok(DbConfig { pairs: Vec::new() })
+        }
+    }
+}
+
+async fn set_db(path: &str, db: DbConfig) -> Result<()> {
+    let text = toml::to_string::<DbConfig>(&db)?;
+    fs::write(path, &text).await?;
     Ok(())
 }
